728d6d1f6b0b8dc1aa371350bd63d4cc31af563d
thib-s <thibaur.boissin+git@gmail.com>
2018-01-25 15:28:24 +0100

    wander task

---
+++ floor_nav/missions/test_wander.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# ROS specific imports
+import roslib; roslib.load_manifest('floor_nav')
+import rospy
+from math import *
+from task_manager_lib.TaskClient import *
+
+rospy.init_node('task_client')
+server_node = rospy.get_param("~server","/task_server")
+default_period = rospy.get_param("~period",0.05)
+tc = TaskClient(server_node,default_period)
+rospy.loginfo("Mission connected to server: " + server_node)
+
+
+
+tc.WaitForAuto()
+try:
+    tc.Wander(max_linear_speed=0.3, max_angular_speed=0.3, dont_care_range=1.5, safety_range=0.50, front_sector=False
+              )
+
+except TaskException, e:
+    rospy.logerr("Exception caught: " + str(e))
+
+if not rospy.core.is_shutdown():
+    tc.SetManual()
+
+
+rospy.loginfo("Mission completed")
