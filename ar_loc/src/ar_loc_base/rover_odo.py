611d5eb8eec31dd1a9a06d1951f4a1c3442d6f60
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-02-08 15:27:11 +0100

    first version of predict

--- ar_loc/src/ar_loc_base/rover_odo.py
+++ ar_loc/src/ar_loc_base/rover_odo.py
@@ -1,4 +1,6 @@
-import roslib; roslib.load_manifest('ar_loc')
+import roslib;
+
+roslib.load_manifest('ar_loc')
 import rospy
 from numpy import *
 from numpy.linalg import pinv, inv
@@ -10,6 +12,7 @@ import threading
 
 from rover_kinematics import *
 
+
 class RoverOdo(RoverKinematics):
     def __init__(self, initial_pose, initial_uncertainty):
         RoverKinematics.__init__(self)
@@ -17,11 +20,13 @@ class RoverOdo(RoverKinematics):
         self.X = mat(vstack(initial_pose))
 
     def getRotation(self, theta):
-        R = mat(zeros((2,2)))
-        R[0,0] = cos(theta); R[0,1] = -sin(theta)
-        R[1,0] = sin(theta); R[1,1] = cos(theta)
+        R = mat(zeros((2, 2)))
+        R[0, 0] = cos(theta);
+        R[0, 1] = -sin(theta)
+        R[1, 0] = sin(theta);
+        R[1, 1] = cos(theta)
         return R
-    
+
     def predict(self, motor_state, drive_cfg, encoder_precision):
         self.lock.acquire()
         # The first time, we need to initialise the state
@@ -29,48 +34,47 @@ class RoverOdo(RoverKinematics):
             self.motor_state.copy(motor_state)
             self.first_run = False
             self.lock.release()
-            return 
+            return
         # then compute odometry using least square
         iW = self.prepare_inversion_matrix(drive_cfg)
-        S = self.prepare_displacement_matrix(self.motor_state,motor_state,drive_cfg)
+        S = self.prepare_displacement_matrix(self.motor_state, motor_state, drive_cfg)
         self.motor_state.copy(motor_state)
-        
+
         # Implement Kalman prediction here
-        theta = self.X[2,0]
-        Rtheta = mat([[cos(theta), -sin(theta), 0], 
-                      [sin(theta),  cos(theta), 0],
-                      [         0,           0, 1]]);
+        theta = self.X[2, 0]
+        Rtheta = mat([[cos(theta), -sin(theta), 0],
+                      [sin(theta), cos(theta), 0],
+                      [0, 0, 1]])
         # Estimate DeltaX using the pseudo-inverse method
-        DeltaX = iW*S
-        self.X = self.X + Rtheta*DeltaX
+        DeltaX = iW * S
+        self.X = self.X + Rtheta * DeltaX
         self.lock.release()
 
     def update_ar(self, Z, L, uncertainty):
         # Odometry does not care about AR tags
-        return 
+        return
 
     def update_compass(self, Z, uncertainty):
         # Odometry does not care about compass
-        return 
+        return
 
-    # Publish the pose for rviz
+        # Publish the pose for rviz
+
     def publish(self, pose_pub, target_frame, stamp):
         pose = PoseStamped()
         pose.header.frame_id = target_frame
         pose.header.stamp = stamp
-        pose.pose.position.x = self.X[0,0]
-        pose.pose.position.y = self.X[1,0]
+        pose.pose.position.x = self.X[0, 0]
+        pose.pose.position.y = self.X[1, 0]
         pose.pose.position.z = 0.0
-        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2,0])
+        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0])
         pose.pose.orientation.x = Q[0]
         pose.pose.orientation.y = Q[1]
         pose.pose.orientation.z = Q[2]
         pose.pose.orientation.w = Q[3]
         pose_pub.publish(pose)
 
-    def broadcast(self,br, target_frame, stamp):
-        br.sendTransform((self.X[0,0], self.X[1,0], 0),
-                     tf.transformations.quaternion_from_euler(0, 0, self.X[2,0]),
-                     stamp, "/%s/ground"%self.name, target_frame)
-        
-
+    def broadcast(self, br, target_frame, stamp):
+        br.sendTransform((self.X[0, 0], self.X[1, 0], 0),
+                         tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0]),
+                         stamp, "/%s/ground" % self.name, target_frame)
--- ar_loc/src/ar_loc_base/rover_pf.py
+++ ar_loc/src/ar_loc_base/rover_pf.py
@@ -1,6 +1,9 @@
-import roslib; roslib.load_manifest('ar_loc')
+import roslib;
+
+roslib.load_manifest('ar_loc')
 import rospy
-from numpy import *
+# from numpy import *
+import numpy as np
 from numpy.linalg import pinv, inv
 from math import pi, sin, cos
 from geometry_msgs.msg import *
@@ -10,30 +13,32 @@ import threading
 
 from rover_kinematics import *
 
+
 class RoverPF(RoverKinematics):
     def __init__(self, initial_pose, initial_uncertainty):
         RoverKinematics.__init__(self)
         self.initial_uncertainty = initial_uncertainty
         self.lock = threading.Lock()
-        self.X = mat(vstack(initial_pose))
+        self.X = np.mat(np.vstack(initial_pose))
         # Initialisation of the particle cloud around the initial position
         self.N = 500
-        self.particles = [self.X + self.drawNoise(initial_uncertainty) for i in range(0,self.N)]
-        self.pa_pub = rospy.Publisher("~particles",PoseArray,queue_size=1)
+        self.particles = [self.X + self.drawNoise(initial_uncertainty) for i in range(0, self.N)]
+        self.pa_pub = rospy.Publisher("~particles", PoseArray, queue_size=1)
 
     def getRotation(self, theta):
-        R = mat(zeros((2,2)))
-        R[0,0] = cos(theta); R[0,1] = -sin(theta)
-        R[1,0] = sin(theta); R[1,1] = cos(theta)
+        R = np.mat(np.zeros((2, 2)))
+        R[0, 0] = cos(theta)
+        R[0, 1] = -sin(theta)
+        R[1, 0] = sin(theta)
+        R[1, 1] = cos(theta)
         return R
-    
+
     # Draw a vector uniformly around [0,0,0], scaled by norm
     def drawNoise(self, norm):
-        if type(norm)==list:
-            return mat(vstack(norm)*(2*random.rand(3,1)-vstack([1,1,1])))
+        if type(norm) == list:
+            return np.mat(np.vstack(norm) * (2 * np.random.rand(3, 1) - np.np.vstack([1, 1, 1])))
         else:
-            return mat(multiply(norm,((2*random.rand(3,1)-vstack([1,1,1])))))
-
+            return np.mat(np.multiply(norm, ((2 * np.random.rand(3, 1) - np.vstack([1, 1, 1])))))
 
     def predict(self, motor_state, drive_cfg, encoder_precision):
         self.lock.acquire()
@@ -42,14 +47,23 @@ class RoverPF(RoverKinematics):
             self.motor_state.copy(motor_state)
             self.first_run = False
             self.lock.release()
-            return 
+            return
         # Prepare odometry matrices (check rover_odo.py for usage)
         iW = self.prepare_inversion_matrix(drive_cfg)
-        S = self.prepare_displacement_matrix(self.motor_state,motor_state,drive_cfg)
+        S = self.prepare_displacement_matrix(self.motor_state, motor_state, drive_cfg)
         self.motor_state.copy(motor_state)
 
         # Apply the particle filter prediction step here
-        # TODO
+        
+        # Estimate DeltaX using the pseudo-inverse method
+        DeltaX = iW * S
+        for p in self.particles:
+            theta = p[2, 0]
+            Rtheta = np.mat([[cos(theta), -sin(theta), 0],
+                             [sin(theta), cos(theta),  0],
+                             [0,          0,           1]])
+            p += DeltaX * Rtheta
+            p += np.random.normal(0.0, encoder_precision, (3, 1))
 
         # self.particles = ...
 
@@ -57,34 +71,36 @@ class RoverPF(RoverKinematics):
 
     def update_ar(self, Z, L, Uncertainty):
         self.lock.acquire()
-        print "Update: L="+str(L.T)
+        print "Update: L=" + str(L.T)
         # Implement particle filter update using landmarks here
         # Note: the function bisect.bisect_left could be useful to implement
         # the resampling process efficiently
         # TODO
+        # for p in self.particles:
+
 
         # self.particles = ...
-        
+
         self.lock.release()
 
     def update_compass(self, angle, Uncertainty):
         self.lock.acquire()
-        print "Update: C="+str(angle)
+        print "Update: C=" + str(angle)
         # Implement particle filter update using landmarks here
         # Note: the function bisect.bisect_left could be useful to implement
         # the resampling process efficiently
         # TODO
 
         # self.particles = ...
-        
+
         self.lock.release()
 
     def updateMean(self):
-        X = mat(zeros((3,1)))
+        X = np.mat(np.zeros((3, 1)))
         for x in self.particles:
             X += x
         self.X = X / len(self.particles)
-        
+
         return self.X
 
     def publish(self, pose_pub, target_frame, stamp):
@@ -93,10 +109,10 @@ class RoverPF(RoverKinematics):
         pose = PoseStamped()
         pose.header.frame_id = target_frame
         pose.header.stamp = stamp
-        pose.pose.position.x = self.X[0,0]
-        pose.pose.position.y = self.X[1,0]
+        pose.pose.position.x = self.X[0, 0]
+        pose.pose.position.y = self.X[1, 0]
         pose.pose.position.z = 0.0
-        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2,0])
+        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0])
         pose.pose.orientation.x = Q[0]
         pose.pose.orientation.y = Q[1]
         pose.pose.orientation.z = Q[2]
@@ -107,16 +123,14 @@ class RoverPF(RoverKinematics):
         pa.header = pose.header
         for p in self.particles:
             po = Pose()
-            po.position.x = p[0,0]
-            po.position.y = p[1,0]
-            q = tf.transformations.quaternion_from_euler(0, 0, p[2,0])
+            po.position.x = p[0, 0]
+            po.position.y = p[1, 0]
+            q = tf.transformations.quaternion_from_euler(0, 0, p[2, 0])
             po.orientation = Quaternion(*q)
             pa.poses.append(po)
         self.pa_pub.publish(pa)
 
-    def broadcast(self,br, target_frame, stamp):
-        br.sendTransform((self.X[0,0], self.X[1,0], 0),
-                     tf.transformations.quaternion_from_euler(0, 0, self.X[2,0]),
-                     stamp, "/%s/ground"%self.name, target_frame)
-        
-
+    def broadcast(self, br, target_frame, stamp):
+        br.sendTransform((self.X[0, 0], self.X[1, 0], 0),
+                         tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0]),
+                         stamp, "/%s/ground" % self.name, target_frame)
