1c37aa55a97940001d33f1451efd8b534febf684
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-02-08 14:02:58 +0100

    update on angle problem

--- rover_driver/src/rover_driver/rover_kinematics.py
+++ rover_driver/src/rover_driver/rover_kinematics.py
@@ -50,20 +50,31 @@ class RoverKinematics:
                 # each wheel in skid-steer mode
                 ground_speed_x = (w.y * twist.angular.z) + twist.linear.x
                 motors.steering[k] = 0
-                motors.drive[k] = ground_speed_x / w.radius * 2 * pi
+                motors.drive[k] = ground_speed_x / w.radius
         else:
             for k, w in drive_cfg.items():
                 # Insert here the steering and velocity of 
                 # each wheel in rolling-without-slipping mode
-                ground_speed_x = (w.y * twist.angular.z) + twist.linear.x
+                ground_speed_x = (w.y * twist.angular.z) - twist.linear.x
                 ground_speed_y = (w.x * twist.angular.z) + twist.linear.y
                 angle = -atan2(ground_speed_y, ground_speed_x)
-                speed = hypot(ground_speed_x, ground_speed_y)
-                if fabs(fmod(angle - self.motor_state.steering[k], 2 * pi)) > pi / 2.0:
+                speed = -hypot(ground_speed_x, ground_speed_y)
+
+                delta_angle = fmod(angle - self.motor_state.steering[k], pi * 2.0)
+                if delta_angle > pi:
+                    delta_angle = 2.0 * pi - delta_angle
+                elif delta_angle < -pi:
+                    delta_angle = -2.0 * pi - delta_angle
+                if delta_angle > pi / 2.0:
+                    # print("correcting wheel", k)
                     speed = -speed
-                    angle = fmod(angle + pi, 2 * pi)
+                    angle = angle - pi
+                elif delta_angle < -pi / 2.0:
+                    speed = -speed
+                    angle = angle + pi
+
                 motors.steering[k] = angle
-                motors.drive[k] = speed / w.radius * 2 * pi
+                motors.drive[k] = speed / w.radius
         return motors
 
     def integrate_odometry(self, motor_state, drive_cfg):
