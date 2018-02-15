a9c38666d798f88d5ecd3f09a7f0ffa0e086b08a
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-02-15 14:14:44 +0100

    correct code presentation to pep8

--- ar_loc/src/ar_loc/rover_kf.py
+++ ar_loc/src/ar_loc/rover_kf.py
@@ -1,4 +1,6 @@
-import roslib; roslib.load_manifest('ar_loc')
+import roslib;
+
+roslib.load_manifest('ar_loc')
 import rospy
 from numpy import *
 from numpy.linalg import pinv, inv
@@ -10,21 +12,24 @@ import threading
 
 from rover_kinematics import *
 
+
 class RoverKF(RoverKinematics):
     def __init__(self, initial_pose, initial_uncertainty):
         RoverKinematics.__init__(self)
         self.lock = threading.Lock()
         self.X = mat(vstack(initial_pose))
         self.P = mat(diag(initial_uncertainty))
-        self.ellipse_pub = rospy.Publisher("~ellipse",Marker,queue_size=1)
-        self.pose_with_cov_pub = rospy.Publisher("~pose_with_covariance",PoseWithCovarianceStamped,queue_size=1)
+        self.ellipse_pub = rospy.Publisher("~ellipse", Marker, queue_size=1)
+        self.pose_with_cov_pub = rospy.Publisher("~pose_with_covariance", PoseWithCovarianceStamped, queue_size=1)
 
     def getRotation(self, theta):
-        R = mat(zeros((2,2)))
-        R[0,0] = cos(theta); R[0,1] = -sin(theta)
-        R[1,0] = sin(theta); R[1,1] = cos(theta)
+        R = mat(zeros((2, 2)))
+        R[0, 0] = cos(theta)
+        R[0, 1] = -sin(theta)
+        R[1, 0] = sin(theta)
+        R[1, 1] = cos(theta)
         return R
-    
+
     def predict(self, motor_state, drive_cfg, encoder_precision):
         self.lock.acquire()
         # The first time, we need to initialise the state
@@ -32,12 +37,12 @@ class RoverKF(RoverKinematics):
             self.motor_state.copy(motor_state)
             self.first_run = False
             self.lock.release()
-            return 
-        # Prepare odometry matrices (check rover_odo.py for usage)
+            return
+            # Prepare odometry matrices (check rover_odo.py for usage)
         iW = self.prepare_inversion_matrix(drive_cfg)
-        S = self.prepare_displacement_matrix(self.motor_state,motor_state,drive_cfg)
+        S = self.prepare_displacement_matrix(self.motor_state, motor_state, drive_cfg)
         self.motor_state.copy(motor_state)
-        
+
         # Implement Kalman prediction here
         # TODO
 
@@ -49,7 +54,7 @@ class RoverKF(RoverKinematics):
 
     def update_ar(self, Z, L, uncertainty):
         self.lock.acquire()
-        print "Update: L="+str(L.T)+" X="+str(self.X.T)
+        print "Update: L=" + str(L.T) + " X=" + str(self.X.T)
         # Implement kalman update using landmarks here
         # TODO
         # self.X = 
@@ -58,23 +63,24 @@ class RoverKF(RoverKinematics):
 
     def update_compass(self, Z, uncertainty):
         self.lock.acquire()
-        print "Update: S="+str(Z)+" X="+str(self.X.T)
+        print "Update: S=" + str(Z) + " X=" + str(self.X.T)
         # Implement kalman update using compass here
         # TODO
         # self.X = 
         # self.P = 
         self.lock.release()
-        return 
+        return
 
-    # this publishes the pose but also the pose with covariance and the error ellipse in rviz
+        # this publishes the pose but also the pose with covariance and the error ellipse in rviz
+
     def publish(self, pose_pub, target_frame, stamp):
         pose = PoseWithCovarianceStamped()
         pose.header.frame_id = target_frame
         pose.header.stamp = stamp
-        pose.pose.pose.position.x = self.X[0,0]
-        pose.pose.pose.position.y = self.X[1,0]
+        pose.pose.pose.position.x = self.X[0, 0]
+        pose.pose.pose.position.y = self.X[1, 0]
         pose.pose.pose.position.z = 0.0
-        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2,0])
+        Q = tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0])
         pose.pose.pose.orientation.x = Q[0]
         pose.pose.pose.orientation.y = Q[1]
         pose.pose.pose.orientation.z = Q[2]
@@ -83,10 +89,16 @@ class RoverKF(RoverKinematics):
         psub.header = pose.header
         psub.pose = pose.pose.pose
         pose_pub.publish(psub)
-        C = [0]*36
-        C[ 0] = self.P[0,0]; C[ 1] = self.P[0,1]; C[ 5] = self.P[0,2]
-        C[ 6] = self.P[1,0]; C[ 7] = self.P[1,1]; C[11] = self.P[1,2]
-        C[30] = self.P[2,0]; C[31] = self.P[2,1]; C[35] = self.P[2,2]
+        C = [0] * 36
+        C[0] = self.P[0, 0]
+        C[1] = self.P[0, 1]
+        C[5] = self.P[0, 2]
+        C[6] = self.P[1, 0]
+        C[7] = self.P[1, 1]
+        C[11] = self.P[1, 2]
+        C[30] = self.P[2, 0]
+        C[31] = self.P[2, 1]
+        C[35] = self.P[2, 2]
         pose.pose.covariance = C
         self.pose_with_cov_pub.publish(pose)
         marker = Marker()
@@ -96,19 +108,17 @@ class RoverKF(RoverKinematics):
         marker.type = Marker.SPHERE
         marker.action = Marker.ADD
         marker.pose = pose.pose.pose
-        marker.scale.x = 3*sqrt(self.P[0,0])
-        marker.scale.y = 3*sqrt(self.P[1,1]);
-        marker.scale.z = 0.1;
-        marker.color.a = 1.0;
-        marker.color.r = 1.0;
-        marker.color.g = 1.0;
-        marker.color.b = 0.0;
+        marker.scale.x = 3 * sqrt(self.P[0, 0])
+        marker.scale.y = 3 * sqrt(self.P[1, 1])
+        marker.scale.z = 0.1
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
         self.ellipse_pub.publish(marker)
 
     # broadcast the estimated transform
-    def broadcast(self,br, target_frame, stamp):
-        br.sendTransform((self.X[0,0], self.X[1,0], 0),
-                     tf.transformations.quaternion_from_euler(0, 0, self.X[2,0]),
-                     stamp, "/%s/ground"%self.name, target_frame)
-        
-
+    def broadcast(self, br, target_frame, stamp):
+        br.sendTransform((self.X[0, 0], self.X[1, 0], 0),
+                         tf.transformations.quaternion_from_euler(0, 0, self.X[2, 0]),
+                         stamp, "/%s/ground" % self.name, target_frame)
--- ar_mapping/src/ar_mapping/mapping_kf.py
+++ ar_mapping/src/ar_mapping/mapping_kf.py
@@ -1,4 +1,6 @@
-import roslib; roslib.load_manifest('ar_mapping')
+import roslib;
+
+roslib.load_manifest('ar_mapping')
 import rospy
 from numpy import *
 from numpy.linalg import inv
@@ -10,39 +12,38 @@ import threading
 import rover_driver
 from rover_driver.rover_kinematics import *
 
+
 class Landmark:
-    def __init__(self, Z, X , R):
+    def __init__(self, Z, X, R):
         # Initialise a landmark based on measurement Z, 
         # current position X and uncertainty R
         # TODO
-        self.L =vstack([0,0])
-        self.P =mat([[0,0],[0,0]])
+        self.L = vstack([0, 0])
+        self.P = mat([[0, 0], [0, 0]])
 
-    def update(self,Z, X, R):
+    def update(self, Z, X, R):
         # Update the landmark based on measurement Z, 
         # current position X and uncertainty R
         # TODO
         return
-        
 
 
 class MappingKF:
     def __init__(self):
         self.lock = threading.Lock()
         self.marker_list = {}
-        self.marker_pub = rospy.Publisher("~landmarks",MarkerArray,queue_size=1)
+        self.marker_pub = rospy.Publisher("~landmarks", MarkerArray, queue_size=1)
 
     def update_ar(self, Z, X, Id, uncertainty):
         self.lock.acquire()
-        print "Update: Z="+str(Z.T)+" X="+str(X.T)+" Id="+str(Id)
-        R = mat(diag([uncertainty,uncertainty]))
+        print "Update: Z=" + str(Z.T) + " X=" + str(X.T) + " Id=" + str(Id)
+        R = mat(diag([uncertainty, uncertainty]))
         # Take care of the landmark Id observed as Z from X
         # self.marker_list is expected to be a dictionary of Landmark
         # such that current landmark can be retrieved as self.marker_list[Id] 
         # At initialisation, self.marker_list is empty
         # TODO
         self.lock.release()
-
 
     def publish(self, target_frame, timestamp):
         ma = MarkerArray()
@@ -55,32 +56,32 @@ class MappingKF:
             marker.type = Marker.CYLINDER
             marker.action = Marker.ADD
             Lkf = self.marker_list[id]
-            marker.pose.position.x = Lkf.L[0,0]
-            marker.pose.position.y = Lkf.L[1,0]
+            marker.pose.position.x = Lkf.L[0, 0]
+            marker.pose.position.y = Lkf.L[1, 0]
             marker.pose.position.z = 0
             marker.pose.orientation.x = 0
             marker.pose.orientation.y = 0
             marker.pose.orientation.z = 1
             marker.pose.orientation.w = 0
-            marker.scale.x = max(3*sqrt(Lkf.P[0,0]),0.05)
-            marker.scale.y = max(3*sqrt(Lkf.P[1,1]),0.05)
-            marker.scale.z = 0.5;
-            marker.color.a = 1.0;
-            marker.color.r = 1.0;
-            marker.color.g = 1.0;
-            marker.color.b = 0.0;
-            marker.lifetime.secs=3.0;
+            marker.scale.x = max(3 * sqrt(Lkf.P[0, 0]), 0.05)
+            marker.scale.y = max(3 * sqrt(Lkf.P[1, 1]), 0.05)
+            marker.scale.z = 0.5
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.lifetime.secs = 3.0
             ma.markers.append(marker)
             marker = Marker()
             marker.header.stamp = timestamp
             marker.header.frame_id = target_frame
             marker.ns = "landmark_kf"
-            marker.id = 1000+id
+            marker.id = 1000 + id
             marker.type = Marker.TEXT_VIEW_FACING
             marker.action = Marker.ADD
             Lkf = self.marker_list[id]
-            marker.pose.position.x = Lkf.L[0,0]
-            marker.pose.position.y = Lkf.L[1,0]
+            marker.pose.position.x = Lkf.L[0, 0]
+            marker.pose.position.y = Lkf.L[1, 0]
             marker.pose.position.z = 1.0
             marker.pose.orientation.x = 0
             marker.pose.orientation.y = 0
@@ -90,11 +91,10 @@ class MappingKF:
             marker.scale.x = 1.0
             marker.scale.y = 1.0
             marker.scale.z = 0.2
-            marker.color.a = 1.0;
-            marker.color.r = 1.0;
-            marker.color.g = 1.0;
-            marker.color.b = 1.0;
-            marker.lifetime.secs=3.0;
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 1.0
+            marker.lifetime.secs = 3.0
             ma.markers.append(marker)
         self.marker_pub.publish(ma)
-
