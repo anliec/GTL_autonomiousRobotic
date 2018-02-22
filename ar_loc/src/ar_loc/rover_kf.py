30bca31a30e7b35943a0df9063386bbfbb262b7d
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-02-22 15:42:08 +0100

    localisation is working

--- ar_loc/src/ar_loc/rover_kf.py
+++ ar_loc/src/ar_loc/rover_kf.py
@@ -4,7 +4,7 @@ roslib.load_manifest('ar_loc')
 import rospy
 import numpy as np
 from numpy.linalg import pinv, inv
-from math import pi, sin, cos
+from math import pi, sin, cos, fmod
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 from visualization_msgs.msg import Marker, MarkerArray
 import tf
@@ -25,10 +25,10 @@ class RoverKF(RoverKinematics):
         self.P = np.mat(np.diag(initial_uncertainty))  # 3x3
         self.ellipse_pub = rospy.Publisher("~ellipse", Marker, queue_size=1)
         self.pose_with_cov_pub = rospy.Publisher("~pose_with_covariance", PoseWithCovarianceStamped, queue_size=1)
-        print type(self.X), type(self.P)
         assert type(self.X) == np.matrixlib.defmatrix.matrix and type(self.P) == np.matrixlib.defmatrix.matrix
 
-    def getRotation(self, theta):
+    @staticmethod
+    def getRotation(theta):
         R = np.mat(np.zeros((2, 2)))
         R[0, 0] = cos(theta)
         R[0, 1] = -sin(theta)
@@ -42,17 +42,12 @@ class RoverKF(RoverKinematics):
         Compute the position of L in the X frame
             :param X: position of the rover [[x],[y],[theta]] on world frame
             :param L: position of the landmark [[x],[y]] on world frame
-            :return pos_lm_frame: polar coordinate of L in the X frame (rover frame)
-            :return dist: cartesian coordinate of the vector from X to L (world frame)
+            :return pos_lm_frame: cartesian coordinate of L in the X frame (rover frame) [[x],[y]]
+            :return dist: cartesian coordinate of the vector from X to L (world frame) [[x],[y]]
         """
-        dist = L - X[0:2]
-        pos_lm_rover_frame = np.mat(
-            [
-                [np.linalg.norm(dist, 2)],
-                [atan2(dist[0, 0], dist[1, 0]) - X[2]]
-            ]
-        )
-        return pos_lm_rover_frame, dist
+        dist = np.mat(L - X[0:2])
+        assert dist[0, 0] == L[0, 0] - X[0, 0] and dist[1, 0] == L[1, 0] - X[1, 0]
+        return RoverKF.getRotation(-X[2, 0]) * dist, dist
 
     def predict(self, motor_state, drive_cfg, encoder_precision):
         assert type(self.X) == np.matrixlib.defmatrix.matrix and type(self.P) == np.matrixlib.defmatrix.matrix
@@ -71,6 +66,7 @@ class RoverKF(RoverKinematics):
         # Implement Kalman prediction here
         DeltaX = iW * S
 
+        # TODO: encoder_precision is the error on S
         Q = np.mat(
             [
                 [encoder_precision, 0, 0],
@@ -101,7 +97,7 @@ class RoverKF(RoverKinematics):
 
     def update_ar(self, Z, L, uncertainty):
         """
-        :param Z: observation in the rover frame [[d],[theta]]
+        :param Z: observation in the rover frame [[x],[y]]
         :param L: position of the landmark on world frame [[x],[y]]
         :param uncertainty:
         :return:
@@ -111,35 +107,28 @@ class RoverKF(RoverKinematics):
         # print "Update: L=" + str(L.T) + " X=" + str(self.X.T) + " uncertainty=" + str(uncertainty)
         # Implement kalman update using landmarks here
 
-        y_polar, dist = Z - self.h(self.X, L)
-        y_polar = np.mat(y_polar)
-        # theta = y_polar[1] + self.X[2, 0]
-        # y_cart = np.mat(
-        #     [
-        #         [cos(theta) * y_polar[0, 0]],
-        #         [sin(theta) * y_polar[0, 0]],
-        #         [y_polar[1, 0]]
-        #     ]
-        # )
-        squared_norme = np.sum(np.power(dist[0:2], 2))
-        norme = np.sqrt(squared_norme)
-        H = np.mat(  # TODO: double check H !!! https://www.wolframalpha.com/input/?i=jacobian(%5B%5Bsqrt((a-x)%5E2%2B(b-y)%5E2)%5D,+%5Batan2((b-y),+(a-x))+-+t%5D%5D)
+        y_cart, dist = self.h(self.X, L)
+        y_cart = Z - y_cart
+        theta = self.X[2, 0]
+        # https://www.wolframalpha.com/input/?i=jacobian(%5B%5Bcos(z)*(a-x)+-+sin(z)*(b-y)%5D,+%5Bsin(z)*(a-x)+%2B+cos(z)*(b-y)%5D%5D)
+        H = np.mat(
             [
-                [-dist[0, 0] / norme,        -dist[1, 0] / norme,          0],
-                [dist[1, 0] / squared_norme, -dist[0, 0] / squared_norme, -1]
+                [-cos(theta), -sin(theta),  dist[1, 0] * cos(theta) - dist[0, 0] * sin(theta)],
+                [sin(theta),  -cos(theta), -dist[0, 0] * cos(theta) - dist[1, 0] * sin(theta)]
             ]
         )
         R = np.mat(np.diag([uncertainty] * 2))  # 2x2
         S = H * self.P * H.T + R  # 2x2
         K = self.P * H.T * np.mat(np.linalg.inv(S))  # 2x3
 
-        self.X += K * y_polar  # 1x3
+        self.X += K * y_cart  # 1x3
         self.P = (np.identity(3) - K * H) * self.P  # 3x3
 
         self.lock.release()
+        print(y_cart)
         assert type(self.X) == np.matrixlib.defmatrix.matrix and type(self.P) == np.matrixlib.defmatrix.matrix
         assert type(H) == np.matrixlib.defmatrix.matrix and type(S) == np.matrixlib.defmatrix.matrix
-        assert type(K) == np.matrixlib.defmatrix.matrix and type(y_polar) == np.matrixlib.defmatrix.matrix
+        assert type(K) == np.matrixlib.defmatrix.matrix and type(y_cart) == np.matrixlib.defmatrix.matrix
         assert type(R) == np.matrixlib.defmatrix.matrix
 
     def update_compass(self, Z, uncertainty):
