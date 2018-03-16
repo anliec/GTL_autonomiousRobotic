d30aa6ca7bfef85ecc0a24fcc3e54f7de94a24e2
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-03-16 11:12:42 +0100

    Add test python script for go to base task

---
+++ floor_nav/missions/test_gotobase.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# ROS specific imports
+import roslib
+
+roslib.load_manifest('floor_nav')
+import rospy
+from math import *
+from task_manager_lib.TaskClient import *
+
+rospy.init_node('task_client')
+server_node = rospy.get_param("~server", "/task_server")
+default_period = rospy.get_param("~period", 0.05)
+tc = TaskClient(server_node, default_period)
+rospy.loginfo("Mission connected to server: " + server_node)
+
+smart = True
+vel = 0.5
+ang_vel = 3
+
+tc.WaitForAuto()
+try:
+    gtb = tc.GoToBase(foreground=True)
+except TaskException, e:
+    rospy.logerr("Exception caught: " + str(e))
+
+if not rospy.core.is_shutdown():
+    tc.SetManual()
+
+rospy.loginfo("Mission completed")
