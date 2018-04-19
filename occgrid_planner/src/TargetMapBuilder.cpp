f0ad3385f87516245f86df585157dd1cc946735f
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-04-19 16:08:22 +0200

    cost debuged

--- occgrid_planner/src/TargetMapBuilder.cpp
+++ occgrid_planner/src/TargetMapBuilder.cpp
@@ -37,9 +37,11 @@ GoalHeap TargetMapBuilder::computeGoals(const cv::Mat_<uint8_t> &map, const cv::
                 // if there is more than 1 unknown neighbor this point is frontier point
                 if (unknown_count > 0.0f){
                     // compute it's baseScore and add it to the heap if far enough from the robot
-                    float distSquared = powf(p.x-robotLoc.x, 2) + powf(p.x-robotLoc.x, 2);
+                    int deltaX = p.x - robotLoc.x;
+                    int deltaY = p.y - robotLoc.y;
+                    float distSquared = deltaY * deltaY + deltaX * deltaX;
                     float dividedDistSquared = distSquared / 100.0f;
-                    float baseScore = unknown_count; //* distSquared / (distSquared + 1.0f)*(distSquared + 1.0f); // 0.04 = 1 / 10^2
+                    float baseScore = dividedDistSquared / ((dividedDistSquared + 1.0f)*(dividedDistSquared + 1.0f)); // 0.04 = 1 / 10^2
                     frontierPoints.emplace_back(baseScore, p);
                     mapFrontierPoint_(p) = static_cast<uint8_t>(baseScore*0.256f);
                 }
