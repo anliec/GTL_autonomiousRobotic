82fe6a9697884ab79c0c776024b2c4a6812bd38f
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-04-19 16:34:22 +0200

    enable the various cost modifier back

--- occgrid_planner/src/TargetMapBuilder.cpp
+++ occgrid_planner/src/TargetMapBuilder.cpp
@@ -30,7 +30,7 @@ GoalHeap TargetMapBuilder::computeGoals(const cv::Mat_<uint8_t> &map, const cv::
                 for (int dj = -1; dj <= 1; dj++){
                     for (int di = -1; di <= 1; di++){
                         if (map(cv::Point(i + di, j + dj)) == UNKNOWN) {
-                            unknown_count = 1.0f;
+                            unknown_count += 1.0f;
                         }
                     }
                 }
@@ -41,7 +41,7 @@ GoalHeap TargetMapBuilder::computeGoals(const cv::Mat_<uint8_t> &map, const cv::
                     int deltaY = p.y - robotLoc.y;
                     float distSquared = deltaY * deltaY + deltaX * deltaX;
                     float dividedDistSquared = distSquared / 100.0f;
-                    float baseScore = dividedDistSquared / ((dividedDistSquared + 1.0f)*(dividedDistSquared + 1.0f)); // 0.04 = 1 / 10^2
+                    float baseScore = unknown_count * dividedDistSquared / ((dividedDistSquared + 1.0f)*(dividedDistSquared + 1.0f)); // 0.04 = 1 / 10^2
                     frontierPoints.emplace_back(baseScore, p);
                     mapFrontierPoint_(p) = static_cast<uint8_t>(baseScore*0.256f);
                 }
@@ -51,22 +51,22 @@ GoalHeap TargetMapBuilder::computeGoals(const cv::Mat_<uint8_t> &map, const cv::
     // add the score of neighbors to the base score computed above
     // can be done in n*log(n) but...
     for(HeapElement &p1 : frontierPoints){
-        float score = p1.first;
-//        float score = 0.0f;
-//        for(HeapElement &p2 : frontierPoints){
-//            //if the points are close enough add their score
-//            if((abs(p1.second.x-p2.second.x) + abs(p1.second.y-p2.second.y)) < MAX_NEIGHBORS_DIST){
-//                score += p2.first;
-//            }
-//        }
+//        float score = p1.first;
+        float score = 0.0f;
+        for(HeapElement &p2 : frontierPoints){
+            //if the points are close enough add their score
+            if((abs(p1.second.x-p2.second.x) + abs(p1.second.y-p2.second.y)) < MAX_NEIGHBORS_DIST){
+                score += p2.first;
+            }
+        }
         //multiply computed score by the an factor depending on angle with robot
         //factor is: pi^2 - deltaAngle^2 + 1
         //with deltaAngle the angle between the robot and the current point in [-pi; pi]
-//        cv::Point move_vector = p1.second - robotLoc;
-//        float angle = atan2f(move_vector.y, move_vector.x);
-//        float deltaAngle = fmodf(angle - robotHeading + 4.0f * float(M_PI), float(2.0 * M_PI));
-//        deltaAngle = deltaAngle - float(M_PI);
-//        score *= (M_PI * M_PI) - (deltaAngle * deltaAngle) + 1;
+        cv::Point move_vector = p1.second - robotLoc;
+        float angle = atan2f(move_vector.y, move_vector.x);
+        float deltaAngle = fmodf(angle - robotHeading + 4.0f * float(M_PI), float(2.0 * M_PI));
+        deltaAngle = deltaAngle - float(M_PI);
+        score *= (M_PI * M_PI) - (deltaAngle * deltaAngle) + 1;
         addToHeap(goals, p1.second, score);
     }
 
